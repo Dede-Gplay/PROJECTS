ac5e5b7e5fbc9961d904a94371a9732dce9b11f3
Dede-Gplay <dedegodspower556@gmail.com>
2024-12-29 19:31:27 +0100

    worked

---
+++ Project_3/Page.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById('mobile');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    }) 
+}
